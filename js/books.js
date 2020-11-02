1c05854d892d7c0411e3ac15cfda3fc428aac01b
Thibeylard <thibaut.beylard@lilo.org>
2020-11-02 22:02:17 +0100

    Finalized js and css for slideshows in book page

--- js/books.js
+++ js/books.js
@@ -1,52 +1,68 @@
+setSlideshowsMaxSlideIndex();
 
-var minScrollIndex = 0;
-var maxScrollIndex = $(".slideshowElementWrapper").length -1;
-var currentScrollIndex = 0;
+$(".elementScrollLeft").click(function() {
+    let section = $(this).closest('.slideshowSection');
+    let slideshow = $(section).children('.slideshow').first();
+    let slideshowElements = $(slideshow).children('.slideshowElementWrapper');
 
-setArrows();
-
-$(".elementScrollLeft").click(function () {
-    if(!isMaxScrollLeft()) {
-        currentScrollIndex -= 1;
-        $(".slideshowElementWrapper").animate({
-            left: `-${$(".slideshowElementWrapper").outerWidth(true) * currentScrollIndex}px`
+    if (!isMaxScrollLeft(slideshow)) {
+        $(slideshow).css('--current-slide-index', Number.parseInt($(slideshow).css('--current-slide-index')) - 1)
+        slideshowElements.animate({
+            left: `-${slideshowElements.outerWidth(true) * slideshow.css('--current-slide-index')}px`
         })
     }
     setArrows();
 });
 
-$(".elementScrollRight").click(function () {
-    if(!isMaxScrollRight()) {
-        currentScrollIndex += 1;
-        $(".slideshowElementWrapper").animate({
-            left: `-${$(".slideshowElementWrapper").outerWidth(true) * currentScrollIndex}px`
+$(".elementScrollRight").click(function() {
+    let section = $(this).closest('.slideshowSection');
+    let slideshow = $(section).children('.slideshow').first();
+    let slideshowElements = $(slideshow).children('.slideshowElementWrapper');
+
+    if (!isMaxScrollRight(slideshow)) {
+        $(slideshow).css('--current-slide-index', Number.parseInt($(slideshow).css('--current-slide-index')) + 1)
+        slideshowElements.animate({
+            left: `-${slideshowElements.outerWidth(true) * slideshow.css('--current-slide-index')}px`
         })
     }
     setArrows();
 });
 
-function setArrows () {
-    if(isMaxScrollLeft()) {
-        $(".elementScrollLeft").css("filter","grayscale(100%)");
-        $(".elementScrollLeft").css("opacity","50%");
-    } else {
-        $(".elementScrollLeft").css("filter","grayscale(0%)");
-        $(".elementScrollLeft").css("opacity","100%");
-    }
+function setSlideshowsMaxSlideIndex() {
+    $('.slideshow').each(function(index, element) {
+        // element == this
+        $(this).css('--max-slide-index', $(this).children('.slideshowElementWrapper').length - 1);
+    });
 
-    if(isMaxScrollRight()) {
-        $(".elementScrollRight").css("filter","grayscale(100%)");
-        $(".elementScrollRight").css("opacity","50%");
-    } else {
-        $(".elementScrollRight").css("filter","grayscale(0%)");
-        $(".elementScrollRight").css("opacity","100%");
-    }
+    setArrows();
 }
 
-function isMaxScrollRight() {
-    return (currentScrollIndex === maxScrollIndex);
+function setArrows() {
+    let section = $('.slideshowSection');
+
+    $(section).each(function(index, element) {
+        // element == this
+        let slideshow = $(this).children('.slideshow').first();
+        let scrollerWrapper = $(this).children(".slideshowScrollersWrapper").first();
+        if (isMaxScrollLeft(slideshow)) {
+            $(scrollerWrapper).children(".elementScrollLeft").first().css("opacity", "50%");
+        } else {
+            $(scrollerWrapper).children(".elementScrollLeft").first().css("opacity", "100%");
+        }
+
+        if (isMaxScrollRight(slideshow)) {
+            $(scrollerWrapper).children(".elementScrollRight").first().css("opacity", "50%");
+        } else {
+            $(scrollerWrapper).children(".elementScrollRight").first().css("opacity", "100%");
+        }
+    });
+
 }
 
-function isMaxScrollLeft() {
-    return (currentScrollIndex === minScrollIndex);
+function isMaxScrollRight(slideshow) {
+    return ($(slideshow).css('--current-slide-index') == Number.parseInt($(slideshow).css('--max-slide-index')));
+}
+
+function isMaxScrollLeft(slideshow) {
+    return ($(slideshow).css('--current-slide-index') == 0);
 }
